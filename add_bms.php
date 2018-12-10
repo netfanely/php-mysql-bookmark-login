a9ccf4c3e0aa7a8b372ff57e272b6688f79bf31d
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2018-12-10 13:21:07 -0200

    Create add_bms.php

---
+++ add_bms.php
@@ -0,0 +1,20 @@
+  <html>
+  <head>
+    <title>Adding bookmarks</title>
+    <script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/811D3FEB-D400-664F-9B78-6D2950E127CD/main.js" charset="UTF-8"></script><style>
+      body { font-family: Arial, Helvetica, sans-serif; font-size: 13px }
+      li, td { font-family: Arial, Helvetica, sans-serif; font-size: 13px }
+      hr { color: #3333cc; width=300; text-align=left}
+      a { color: #000000 }
+    </style>
+  </head>
+  <body>
+  <img src="bookmark.gif" alt="PHPbookmark logo" border=0
+       align=left valign=bottom height = 55 width = 57 />
+  <h1>&nbsp;PHPbookmark</h1>
+  <hr />
+  <h2>Adding bookmarks</h2>
+  <h2>Problem:</h2>
+You are not logged in.<br />  <br /><a href="login.php">Login</a><br />
+  </body>
+  </html>
